caef0311bdff2441252d586d1fda33fab789108f
Selvenity <121463752+Selvenity@users.noreply.github.com>
2022-12-31 15:49:21 -0800

    Update help.js

--- help.js
+++ help.js
@@ -11,13 +11,13 @@ $(window).on('xpboot', () => {
       </style>
       <div class="docs_container">
         <h1>
-          RebornXP
+          MeowerOS
         </h1>
-        <p>RebornXP is an effort to bring back the late 2000's personal computing experience right to your desktop.</p>
-        <p>I always remembered the nostalgic feeling of WindowsXP which is now discontinued and unfortunately gone from everyone's heart.</p>
+        <p>MeowerOS is a Meower Community Project to make a full-fledged Meower Operating System.</p>
+        <p>This is a fun side project for people to work on in their spare time.</p>
         <p>This is just a hobby and Open Source Project, and there are not many people to support its development, so it lacks some good features.</p>
-        <p>RebornXP is open source, and I would love to accept forks and pull requests on <a href="https://github.com/shoaib-jamal/reborn-xp">GitHub</a></p>
-        <p>You are free to use RebornXP, or parts of RebornXP in your projects.</p>
+        <p>MeowerOS is open source, and I would love to accept forks and pull requests on <a href="https://github.com/MeowerOS/MeowerOS-Javascript/">GitHub</a></p>
+        <p>You are free to use MeowerOS, or parts of MeowerOS in your projects.</p>
         <img src="xpthrives.jpg" width="50%" height="50%">
         <p><b>Disclamier:<br>The Windows XP name, artwork, trademark are surely property of Microsoft. This project is provided for educational purposes only. It is not affiliated with and has not been approved by Microsoft.</p></b>
       </div>
