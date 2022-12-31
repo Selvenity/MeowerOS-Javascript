e97b97a601c28dccae7d4bc5e8537730b0cc8538
Arrow <89811520+ArrowAced@users.noreply.github.com>
2022-12-31 18:50:24 -0500

    overthrow the

--- setup.js
+++ setup.js
@@ -36,7 +36,7 @@ win.content(`
     <br/>
     <p style="margin-left:64px;">
         <table style="font-size:11px;color:#000;">
-            <tr><td rowspan="4" style="width:72px;"><img class="userimg" src="https://cdn.glitch.com/01d2e04f-e49d-4304-aa9e-55b9849b4cce%2Fuser.bmp?1522560745448"/></td></tr>
+            <tr><td rowspan="4" style="width:72px;"><img class="userimg" src="https://cdn.glitch.com/01d2e04f-e49d-4304-aa9e-55b9849b4cce%2Fusertile8.bmp?1522560745448"/></td></tr>
             <tr><td>Type a user name (for example, Meowy)</td></tr>
             <tr><td><input type="text" class="username" style="width:180px;"/></td></tr>
             <tr><td>&nbsp;</td></tr>
